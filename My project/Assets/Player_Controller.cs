08cb498d13d1db5efe9c483c443a2d88f507306f
harelmarin <145020856+harelmarin@users.noreply.github.com>
2025-01-17 16:59:51 +0100

    [ADD] Player

---
+++ My project/Assets/Player_Controller.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+    using UnityEngine.InputSystem;
+    public class Player_Controller : MonoBehaviour
+    {
+
+        [SerializeField]private int speed = 1;
+        [SerializeField]private float jumpforce = 10;
+
+
+
+        private IA_Player myInputActions;
+        private InputAction moveAction;
+        private InputAction jumpAction;
+        private Rigidbody rb;
+
+
+
+        void Awake() {
+            myInputActions =  new IA_Player();
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void OnEnable() {
+            moveAction = myInputActions.Movements.Move;
+            moveAction.Enable();
+            jumpAction = myInputActions.Movements.Jump;
+            jumpAction.performed += OnJump;
+            jumpAction.Enable();
+        }
+        private void OnDisable() {
+            moveAction.Disable();
+            myInputActions.Movements.Jump.Disable();
+        }
+
+        void Start()
+        {
+
+        }
+
+        private void OnJump(InputAction.CallbackContext callbackContext) {
+            rb.AddForce(UnityEngine.Vector3.up * jumpforce, ForceMode.Impulse);
+        }
+
+        void FixedUpdate() {
+            UnityEngine.Vector2 moveDir = moveAction.ReadValue<UnityEngine.Vector2>(); 
+            UnityEngine.Vector3 vel =  rb.linearVelocity;
+            vel.x = moveDir.x * speed; 
+            vel.z = moveDir.y * speed;
+            rb.linearVelocity = vel;
+        }
+
+    }
